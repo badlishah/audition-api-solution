2f1791369b3267e3e2e77b8a7f0fab8983ff6bfb
Badlishah Jamil <badlishah@gmail.com>
2024-08-12 10:21:10 +1000

    Cleanup

--- src/main/java/com/audition/integration/AuditionIntegrationClient.java
+++ src/main/java/com/audition/integration/AuditionIntegrationClient.java
@@ -1,14 +1,11 @@
 package com.audition.integration;
 
-import com.audition.common.exception.SystemException;
 import com.audition.model.AuditionComment;
 import com.audition.model.AuditionPost;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -30,28 +27,15 @@ public class AuditionIntegrationClient {
     String commentPath;
 
     public List<AuditionPost> getPosts() {
-        // TODO make RestTemplate call to get Posts from https://jsonplaceholder.typicode.com/posts
         final String postUrl = baseUrl + "/" + postPath;
         final var responseEntity = restTemplate.getForEntity(postUrl, AuditionPost[].class);
         return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
     }
 
     public AuditionPost getPostById(final Integer id) {
-        // TODO get post by post ID call from https://jsonplaceholder.typicode.com/posts/
-        try {
-            final String postUrl = baseUrl + "/" + postPath + "/" + id;
-            final var responseEntity = restTemplate.getForEntity(postUrl, AuditionPost.class);
-            final var auditionPostItem = responseEntity.getBody();
-            return auditionPostItem;
-        } catch (final HttpClientErrorException e) {
-            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new SystemException("Cannot find a Post with id " + id, "Resource Not Found",
-                    404);
-            } else {
-                // TODO Find a better way to handle the exception so that the original error message is not lost. Feel free to change this function.
-                throw new SystemException("Unknown Error message");
-            }
-        }
+        final String postUrl = baseUrl + "/" + postPath + "/" + id;
+        final var responseEntity = restTemplate.getForEntity(postUrl, AuditionPost.class);
+        return responseEntity.getBody();
     }
 
     // TODO Write a method GET comments for a post from https://jsonplaceholder.typicode.com/posts/{postId}/comments - the comments must be returned as part of the post.
@@ -66,7 +50,6 @@ public class AuditionIntegrationClient {
         return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
     }
 
-    // TODO write a method. GET comments for a particular Post from https://jsonplaceholder.typicode.com/comments?postId={postId}.
     // The comments are a separate list that needs to be returned to the API consumers. Hint: this is not part of the AuditionPost pojo.
     public List<AuditionComment> getCommentsFromParamId(final Integer postId) {
         String commentsUrl = baseUrl + "/"
