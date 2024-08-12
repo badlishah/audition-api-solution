108e95838ed383267e3da583fdb3e2d01dd5d44d
Badlishah Jamil <badlishah@gmail.com>
2024-08-12 10:12:55 +1000

    Added open telemetry trace and span id

--- src/main/java/com/audition/configuration/ResponseHeaderInjector.java
+++ src/main/java/com/audition/configuration/ResponseHeaderInjector.java
@@ -1,10 +1,23 @@
 package com.audition.configuration;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class ResponseHeaderInjector {
+public class ResponseHeaderInjector extends OncePerRequestFilter {
+    private static final String TRACE_ID = "X-openTelemetryTraceId";
+    private static final String SPAN_ID = "X-openTelemetrySpanId";
 
-    // TODO Inject openTelemetry trace and span Ids in the response headers.
-
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        response.setHeader(TRACE_ID, "-");
+        response.setHeader(SPAN_ID, "-");
+        chain.doFilter(request, response);
+    }
 }
